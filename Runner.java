59792bb6cdb71c10997570d312a20eeaebcab580
pranav <prnvkatkar88@gmail.com>
2021-10-18 12:40:38 +0530

    Added:created a simple linked list of 56,30,70

--- Runner.java
+++ Runner.java
@@ -2,7 +2,11 @@
 public class Runner {
 
 	public static void main(String[] args) {
-		System.out.println("welcome to linkedlist");
+		LinkedList list = new LinkedList();
+		list.insert(56);
+		list.insert(30);
+		list.insert(70);
+		list.show();
 	}
 
 }
