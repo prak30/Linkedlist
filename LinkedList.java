f65ec6874078559a267809e1e68d719834e87939
pranav <prnvkatkar88@gmail.com>
2021-10-18 12:48:18 +0530

    Added:created linked list by adding 30 and 56 to 70

--- LinkedList.java
+++ LinkedList.java
@@ -26,4 +26,12 @@ public class LinkedList {
 			n.next = node;
 		}
 	}
+
+	public void insertAtStart(int data) {
+		Node node = new Node();
+		node.data = data;
+		node.next = null;
+		node.next = head;
+		head = node;
+	}
 }
